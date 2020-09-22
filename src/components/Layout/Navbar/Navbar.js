9b1522397fb718b0580fa72672d157a8c3e8229f
git config --global user.name <abdullah.bayomy11@gmail.com>
2020-09-22 18:35:23 +0200

    responsive css for mobile

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -9,7 +9,11 @@ const navbar = (props) => {
       <ul>
         <li>
           <Link to='/'>Home</Link>
+        </li>
+        <li>
           <Link to='/about'>About</Link>
+        </li>
+        <li>
           <Link to='/contact'>Contact</Link>
         </li>
       </ul>
